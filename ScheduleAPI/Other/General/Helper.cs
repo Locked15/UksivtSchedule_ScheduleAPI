e6c5e71b26317010163f7ffc7058039628f7899f
Алмаз <almgal353@gmail.com>
2022-06-16 16:52:49 +0500

    User-agent property updated.

--- ScheduleAPI/Other/General/Helper.cs
+++ ScheduleAPI/Other/General/Helper.cs
@@ -57,7 +57,7 @@ namespace ScheduleAPI.Other.General
                 try
                 {
                     client.UseDefaultCredentials = true;
-                    client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.31 Safari/537.36");
+                    client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36");
 
                     while (File.Exists(fileName))
                     {
