4322946653c6da475dbab20851444916429c441d
Locked15 <almgal353@gmail.com>
2022-11-22 15:15:10 +0500

    Added logger for standalone functionality, not dependant on main controllers.

--- ScheduleAPI/Controllers/Views/HomeController.cs
+++ ScheduleAPI/Controllers/Views/HomeController.cs
@@ -8,6 +8,20 @@ namespace ScheduleAPI.Controllers.ViewsControllers
     public class HomeController : Controller
     {
         /// <summary>
+        /// Базовый контроллер для логирования событий, тесно не связанных со специализированными контроллерами.
+        /// </summary>
+        public static ILogger<HomeController>? BaseLogger { get; private set; } = null;
+
+        /// <summary>
+        /// Конструктор класса. Неявно вызывается при первом обращении к API.
+        /// </summary>
+        /// <param name="logger">Настроенный сервис логирования.</param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            BaseLogger = logger;
+        }
+
+        /// <summary>
         /// Функция получения базовой страницы API.
         /// </summary>
         /// <returns>Запрос на отрисовку страницы "Index.cshtml".</returns>
