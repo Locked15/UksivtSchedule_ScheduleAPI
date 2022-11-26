892a73ddb709f763ad0225b5f496a88084482b81
Locked15 <almgal353@gmail.com>
2022-11-26 15:32:54 +0500

    Some updates. Some.

--- ScheduleAPI/Program.cs
+++ ScheduleAPI/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle.
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["AppIns.Connect"]);
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
