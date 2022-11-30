26d96a4c70ce031191ce6e83ce63d5003055e497
Locked15 <almgal353@gmail.com>
2022-12-01 03:16:36 +0500

    Tests project folder rearranged (moved to main project folder).

---
+++ ScheduleAPI-Tests/BasicTests.cs
@@ -0,0 +1,12 @@
+namespace ScheduleAPI_Tests
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void TestingFunctionName_SendSomething_ReturnTargetResult()
+        {
+
+        }
+    }
+}
