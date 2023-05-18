3fe181c3a381ebe7d62b97164dc7f95a8314aa94
Locked15 <almgal353@gmail.com>
2023-05-18 23:01:39 +0500

    Added new utility model entity. Also, created new controller, that allows to get info about actuality of the DB Data.

---
+++ ScheduleAPI/Controllers/API/V2/StatusController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScheduleAPI.Models.Entities;
+using ScheduleAPI.Models.Entities.Utilities;
+
+namespace ScheduleAPI.Controllers.API.V2
+{
+    [Route("~/api/v2/")]
+    [Route("~/api/v2/[controller]")]
+    public class StatusController : Controller
+    {
+        #region Область: Свойства.
+
+        private DataContext DataContext { get; init; }
+        #endregion
+
+        #region Область: Инициализаторы.
+
+        public StatusController(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+        #endregion
+
+        #region Область: Обработчики API.
+
+        [HttpGet]
+        public IActionResult GetDbDataRelevance()
+        {
+            var lastAvailableReplacementDate = DataContext.ScheduleReplacements.Max(replacement =>
+                                                                                    replacement.ReplacementDate);
+            var lastAvailableFinalScheduleDate = DataContext.FinalSchedules.Max(schedule =>
+                                                                                schedule.ScheduleDate);
+
+            var relevanceStatus = new EntitiesDatesStatus(lastAvailableReplacementDate, lastAvailableFinalScheduleDate);
+
+            return Json(relevanceStatus);
+        }
+        #endregion
+    }
+}
