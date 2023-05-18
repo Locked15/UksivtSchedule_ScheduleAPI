8b38550fc3135fbac03b2f227d3e885990cca825
Locked15 <almgal353@gmail.com>
2023-05-18 22:41:18 +0500

    Entity classes updated, to ignore some properties on JSON serialization.

--- ScheduleAPI/Models/Entities/Tables/FinalSchedule.cs
+++ ScheduleAPI/Models/Entities/Tables/FinalSchedule.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Text.Json.Serialization;
 
 namespace ScheduleAPI.Models.Entities.Tables;
 
@@ -7,15 +6,33 @@ public partial class FinalSchedule
 {
     public int Id { get; set; }
 
+    /// <summary>
+    /// Это свойство содержит данные реализации.
+    /// </summary>
+    [JsonIgnore]
     public int CommitHash { get; set; }
 
     public string TargetGroup { get; set; } = null!;
 
+    [JsonIgnore]
     public DateOnly ScheduleDate { get; set; }
 
+    /// <summary>
+    /// Это значение не имеет смысла возвращать.
+    /// Для пользователя целевой цикл ясен из целевой даты.
+    /// </summary>
+    [JsonIgnore]
     public int CycleId { get; set; }
 
+    /// <summary>
+    /// Сериализация приводит к ошибке зацикливания.
+    /// </summary>
+    [JsonIgnore]
     public virtual TargetCycle Cycle { get; set; } = null!;
 
+    /// <summary>
+    /// Сериализация этого значения приводит к ошибке зацикливания.
+    /// </summary>
+    [JsonIgnore]
     public virtual ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
 }
