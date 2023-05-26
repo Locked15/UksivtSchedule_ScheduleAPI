533ff7b9cca704c7a90fd646ac4c05fbedfeb152
Locked15 <almgal353@gmail.com>
2023-05-26 22:14:19 +0500

    Added new entity. Added wrapper for entity result (for group).

---
+++ ScheduleAPI/Models/Entities/Wrappers/Group/GroupBasicScheduleWrapper.cs
@@ -0,0 +1,35 @@
+﻿using ScheduleAPI.Models.Entities.Tables;
+using ScheduleAPI.Models.Entities.Views;
+
+namespace ScheduleAPI.Models.Entities.Wrappers.Group
+{
+    public class GroupBasicScheduleWrapper
+    {
+        public int DayIndex { get; set; }
+
+        public DateOnly TargetDate { get; set; }
+
+        public BasicSchedule? BasicSchedule { get; set; }
+
+        public List<UtilityLessonGroup> LessonsInfo { get; set; }
+
+        public GroupBasicScheduleWrapper(int dayIndex, DateOnly targetDate, BasicSchedule? basicSchedule, List<UtilityLessonGroup> lessonsInfo)
+        {
+            DayIndex = dayIndex;
+            TargetDate = targetDate;
+            BasicSchedule = basicSchedule;
+            LessonsInfo = lessonsInfo;
+
+            ClearLessonsTeacherEmptyProperty();
+        }
+
+        private void ClearLessonsTeacherEmptyProperty()
+        {
+            foreach (var lesson in LessonsInfo)
+            {
+                if (string.IsNullOrWhiteSpace(lesson.LessonTeacher))
+                    lesson.LessonTeacher = null;
+            }
+        }
+    }
+}
