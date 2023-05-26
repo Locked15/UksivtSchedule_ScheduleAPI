b1b60a436d7ac344da1b0e51b4225293be943a8e
Locked15 <almgal353@gmail.com>
2023-05-26 22:13:14 +0500

    Added possibility to create DateOnly object from AtomicDate entity.

--- ScheduleAPI/Models/Entities/Views/UtilityAtomicDate.cs
+++ ScheduleAPI/Models/Entities/Views/UtilityAtomicDate.cs
@@ -5,11 +5,13 @@ namespace ScheduleAPI.Models.Entities.Views;
 
 public partial class UtilityAtomicDate
 {
-    public int? DayOfWeek { get; set; }
+    public int DayOfWeek { get; set; }
 
-    public int? DayOfMonth { get; set; }
+    public int DayOfMonth { get; set; }
 
-    public int? Month { get; set; }
+    public int Month { get; set; }
 
-    public int? Year { get; set; }
+    public int Year { get; set; }
+
+    public DateOnly ToDateOnly() => new(Year, Month, DayOfMonth);
 }
