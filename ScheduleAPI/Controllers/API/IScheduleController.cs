470c010a5388969835beee9fb31fe15043da7ba2
Locked15 <79941113+Locked15@users.noreply.github.com>
2023-11-04 17:32:04 +0500

    Added new basic interface for all schedule-themed controllers.

---
+++ ScheduleAPI/Controllers/API/IScheduleController.cs
@@ -0,0 +1,23 @@
+﻿namespace ScheduleAPI.Controllers.API
+{
+    public interface IScheduleController
+    {
+        #region Область: Параметры выборки расписания.
+
+        protected const int DefaultDayIndex = 0;
+
+        protected const int DefaultTeacherIndex = 0;
+
+        protected const string DefaultGroupName = "20П-3";
+        #endregion
+
+        #region Область: Параметры поиска.
+
+        protected const string DefaultBranch = "Programming";
+
+        protected const string DefaultAffiliation = "П";
+
+        protected const string DefaultTeacherSearchRequest = "Карим";
+        #endregion
+    }
+}
